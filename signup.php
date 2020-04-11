9346303e78f999a7011f54697a5e1567f49dce63
Ahnaf Khan <ahnafkhan@Ahnafs-Air.fios-router.home>
2020-04-11 11:31:06 -0400

    added signup page and corresponding php

--- signup.php
+++ signup.php
@@ -45,14 +45,14 @@
                 </div>
             </div>
         </nav>
-    </header>
+      </header>
         <div class="container">
             <div class="row">
               <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
                 <div class="card card-signin my-5">
                   <div class="card-body">
-                    <form method="post" class="form-signin">
-                      <h1 class="card-title text-center text-uppercase" style="color: #8e44ad">Sign In</h1>
+                    <form method="post" class="form-signin" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                      <h1 class="card-title text-center text-uppercase" style="color: #8e44ad">Sign Up</h1>
                       <!-- <div class="form-label-group">
                         <input type="username" id="{{form.username.auto_id}}" class="form-control" placeholder="Username" required autofocus>
                         <label for="{{form.username.auto_id}}">Username</label>
@@ -69,6 +69,21 @@
                       <!-- {{ form.password }} -->
                       <div class="form-label-group">
                         <!-- {{form.username}} -->
+                        <input id="id_firstName" name="firstName" type="text" class="form-control firstName-button" placeholder="firstName" required autofocus/>
+                        <label for="id_firstName">First Name</label>
+                      </div>
+                      <div class="form-label-group">
+                        <!-- {{form.password}} -->
+                        <input id="id_lastName" name="lastName" type="lastName" class="form-control lastName-button" placeholder="lastName" required/>
+                        <label for="id_lastName">Last Name</label>
+                      </div>
+                      <div class="form-label-group">
+                        <!-- {{form.password}} -->
+                        <input id="id_email" name="email" type="email" class="form-control email-button" placeholder="email" required/>
+                        <label for="id_email">Email</label>
+                      </div>
+                      <div class="form-label-group">
+                        <!-- {{form.username}} -->
                         <input id="id_username" name="username" type="text" class="form-control username-button" placeholder="Username" required autofocus/>
                         <label for="id_username">Username</label>
                       </div>
@@ -78,8 +93,10 @@
                         <label for="id_password">Password</label>
                       </div>
                       <span class="error" id="input-error"></span>
-                      <button class="btn btn-lg btn-block text-uppercase login-button" type="submit" onclick="validateInfo()">Sign in</button>
-                      <a href="./signup.php" type="button" class="btn btn-lg btn-block text-uppercase" style="background-color:white; border-color:#8e44ad; color:#8e44ad; border-width: thin thin thin thin">New User? Signup!</a>                      <!-- <hr class="my-4">
+                      <!-- <button class="btn btn-lg btn-block text-uppercase login-button" type="submit" onclick="validateInfo()">Sign Up</button> -->
+                      
+                      <a href="./login.html" type="button" class="btn btn-lg btn-block text-uppercase" style="background-color:white; border-color:#8e44ad; color:#8e44ad; border-width: thin thin thin thin">Have an Account? Login!</a>
+                      <!-- <hr class="my-4">
                       <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
                       <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button> -->
                     </form>
@@ -88,17 +105,107 @@
               </div>
             </div>
           </div>
+          <?php 
+
+            require('connect-db.php');
+
+            $firstName = $lastName = $password = $username = $email = "";
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+              $firstName = test_input($_POST["firstName"]);
+              $lastName = test_input($_POST["lastName"]);
+              $email = test_input($_POST["email"]);
+              $username = test_input($_POST["username"]);
+              $password = test_input($_POST["password"]);
+            }
+
+            function test_input($data) {
+              $data = trim($data);
+              $data = stripslashes($data);
+              $data = htmlspecialchars($data);
+              return $data;
+            }
+
+            $query = "INSERT INTO user(firstName, lastName, username, password, email) VALUES (:firstName, :lastName, :username, :password, :email)";
+            $statement = $db->prepare($query);
+            $statement->bindValue(':firstName', $firstName);
+            $statement->bindValue(':lastName', $lastName);
+            $statement->bindValue(':username', $username);
+            $statement->bindValue(':password', $password);
+            $statement->bindValue(':email', $email);
+            $statement->execute();
+            $statement->closeCursor();
+
+            // echo $firstName;
+            // echo "<br>";
+            // echo $lastName;
+            // echo "<br>";
+            // echo $email;
+            // echo "<br>";
+            // echo $username;
+            // echo "<br>";
+            // echo $password;
+            // function insertData()
+            // {
+            //    require('connect-db.php');
+               
+            //    $course_id = "newid_from_insertData";
+            //    $course_desc = "newdesc_from_insertData";
+               
+            //    $query = "INSERT INTO courses (courseID, course_desc) VALUES (:course_id, :course_desc)";
+            //    $firstName = "Shahiiiira";
+            //    $lastName = "Ali";
+            //    $username = "Falling";
+            //    $password = "LuvOpenLettersss<3";
+            //    $email = "monkeyasss@gmail.com";
+
+            //   $firstName = $lastName = $password = $username = $email = "";
+
+            //   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            //     $firstName = test_input($_POST["firstName"]);
+            //     $lastName = test_input($_POST["lastName"]);
+            //     $email = test_input($_POST["email"]);
+            //     $username = test_input($_POST["username"]);
+            //     $password = test_input($_POST["password"]);
+            //   }
+
+            //   function test_input($data) {
+            //     $data = trim($data);
+            //     $data = stripslashes($data);
+            //     $data = htmlspecialchars($data);
+            //     return $data;
+            //   }
+               
+
+
+            //    $query = "INSERT INTO user(firstName, lastName, username, password, email) VALUES (:firstName, :lastName, :username, :password, :email)";
+            //    $statement = $db->prepare($query);
+            //    $statement->bindValue(':firstName', $firstName);
+            //    $statement->bindValue(':lastName', $lastName);
+            //    $statement->bindValue(':username', $username);
+            //    $statement->bindValue(':password', $password);
+            //    $statement->bindValue(':email', $email);
+            //    $statement->execute();
+            //    $statement->closeCursor();
+            // }
+          ?>
           <script>
               var validateInfo = function(){
-                  var us = document.getElementById("id_username").value;
-                  var pass = document.getElementById("id_password").value;
+                var fN = document.getElementById("id_firstName").value;
+                var lN = document.getElementById("id_lastName").value;
+                var em = document.getElementById("id_email").value;
+                var us = document.getElementById("id_username").value;
+                var pass = document.getElementById("id_password").value;
 
-                  if( us == "" || pass == "" ){
-                        document.getElementById("id_username").focus();
-                        document.getElementById("id_password").focus();
-                        alert("Neither username nor password can be left blank");
-                        //document.getElementById("input-error").innerHTML = "Neither username nor password can be left blank";
-                  } 
+                if( us == "" || pass == "" || fN == "" || lN == "" || em == ""){
+                    document.getElementById("id_firstName").focus();
+                    document.getElementById("id_lastName").focus();
+                    document.getElementById("id_email").focus();
+                    document.getElementById("id_username").focus();
+                    document.getElementById("id_password").focus();
+                    alert("None of the fields can be left blank");
+                    //document.getElementById("input-error").innerHTML = "Neither username nor password can be left blank";
+                } 
                 //   else if( us = "" && pass != ""){
                 //         document.getElementById("id_username").focus();
                 //         document.getElementById("username-error").innerHTML = "Username cannot be blank";
