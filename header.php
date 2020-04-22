9f58e0a9b822276be6db519d6b6860e8d3b9ea65
Ahnaf Khan <ahnafkhan@Ahnafs-MacBook-Air.local>
2020-04-21 20:36:31 -0400

    php changes

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #8e44ad;">
         <div class="container-fluid">
-            <a class="navbar-brand" href="./index.php">ReciBlogssss</a>
+            <a class="navbar-brand" href="./index.php">ReciBlog</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
